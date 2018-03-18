f7db289e43b775b4269522063c84ad507eab5cce
Artem Angaev <ololok@mail.ua>
2018-03-18 21:46:34 +0300

    Добавил тесты к жадному алгоритму

--- Lab1/Рюкзак/backpack-L1T6/backpack-L1T6/test.bat
+++ Lab1/Рюкзак/backpack-L1T6/backpack-L1T6/test.bat
@@ -11,11 +11,36 @@ if NOT ERRORLEVEL 1 goto err
 rem Размер рюкзака - 15, список вещей - set
 %PROGRAM% 15 set.txt > "%TEMP%\backPack.txt"
 if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\backPack.txt" "test-data\out1.txt" >nul
+if ERRORLEVEL 1 goto err
 
+rem 10 предметов ценой 1 и весом 1, рюкзак вместимостью 10, в результате возьмет все 
+%PROGRAM% 10 10.txt > "%TEMP%\10.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\10.txt" "test-data\out2.txt" >nul
+if ERRORLEVEL 1 goto err
+
+rem все предметы слишком большие для рюкзака
+%PROGRAM% 0 set.txt > "%TEMP%\backPack.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\backPack.txt" "test-data\out3.txt" >nul
+if ERRORLEVEL 1 goto err
+
+rem  Дорогой предмет (100 , вес >= 1/2 грузоподъемности), и другие (вес > 1/2 рюкзака, цена дешевка), 
+rem в результате в рюкзаке будет один дорогой предмет и пустое место
+%PROGRAM% 30 set4.txt > "%TEMP%\backPack.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\backPack.txt" "test-data\out4.txt" >nul
+if ERRORLEVEL 1 goto err
+
+rem Это не жадный алгоритм (в out5.txt жадное поведение)
+%PROGRAM% 80 set5.txt > "%TEMP%\backPack.txt"
+if ERRORLEVEL 1 goto err
+fc.exe "%TEMP%\backPack.txt" "test-data\out5.txt" >nul
+if not ERRORLEVEL 1 goto err
 
 echo Testing pass!
 exit 0
-
 
 :err
 echo Program testing failed
